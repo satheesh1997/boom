41f6cd1b1c9595730a39086f6254a084d7ab7a52
Satheesh Kumar <mail@satheesh.dev>
2023-04-01 22:01:31 +0530

    chore: register /games routes in router

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -2,11 +2,23 @@ package main
 
 import (
     "log"
+
     "github.com/gin-gonic/gin"
+    "github.com/satheesh1997/boom/games"
 )
 
 func setupRouter() *gin.Engine {
     router := gin.Default()
+
+    // Controllers
+    gamesController := games.NewController()
+
+    // Routes for /games
+    gamesGroup := router.Group("/games")
+    {
+        gamesGroup.POST("/flames", gamesController.ComputeFlames)
+    }
+
     return router
 }
 
